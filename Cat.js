58feb0596d00ec963c6fede81bf39dd890de5546
sml <dongluu.lqd@gmail.com>
2020-09-05 11:03:21 -0700

    add stomach properties and eat function in Cat module; and alive properties and dead function in Mouse module

--- Cat.js
+++ Cat.js
@@ -1,4 +1,8 @@
 function Cat(){
-
+  this.stomach = [];
+  this.eat = function(mouse){
+    stomach.push(mouse);
+    mouse.dead();
+  };
 }
 module.exports = Cat;
--- Mouse.js
+++ Mouse.js
@@ -1,5 +1,9 @@
 funtion Mouse(name){
   this.name = name;
+  this.alive = true;
+  this.dead = function(){
+    this.alive = false;
+  }
 }
 
 module.exports = Mouse;
