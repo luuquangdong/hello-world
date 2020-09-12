a0dd40dde147bf2af07541085d75e164e2643377
sml <me@sml.com>
2020-09-12 22:36:01 +0700

    add index, main, Bird class

--- Bird.js
+++ Bird.js
@@ -1,6 +1,10 @@
-function Bird(){
+export function Bird(){
 }
 
 Bird.prototype.fly = function(){
   console.log("i am flying");
+}
+
+Bird.prototype.sleep = function(){
+  console.log("i am sleeping");
 }
