f344ecbf319237cff926a1406354c56f0325b7af
sml <me@sml.com>
2020-09-12 23:21:15 +0700

    say method for mouse
    A
    B

--- Cat.js
+++ Cat.js
@@ -8,5 +8,7 @@ Cat.prototype.eat = function(mouse){
 Cat.protorype.say = function(){
   console.log("mew mew");
 }
-
+Cat.prototype.sleep = function(){
+  console.log("i am sleeping");
+}
 module.exports = Cat;
--- Mouse.js
+++ Mouse.js
@@ -6,8 +6,11 @@ funtion Mouse(name){
 Mouse.prototype.dead = function(){
   this.alive = false;
 }
-
 Mouse.prototype.sleep = function(){
   console.log("i am sleeping");
 }
+Mouse.prototype.say = function(){
+  console.log("chit chit");
+}
+
 module.exports = Mouse;
